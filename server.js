14f24813f01df849ae4483bf33aba17fcb9b08fe
Daniel Dimitrov <ayyayyedev@gmail.com>
2019-03-25 16:41:20 +0200

    Better formatting

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const WebSocket = require('ws');
 const discord = require('./discord');
 
-const wss = new WebSocket.Server({ port: 6969 });
+const wss = new WebSocket.Server({
+  port: 6969
+});
 
 let lastSong = null;
 
 wss.on('connection', function connection(ws) {
-     ws.on('message', function incoming(title) {
-         if(!title || lastSong === title || title === "YouTube") return;
-         lastSong = title;
-         discord.send(title);
-    });
-    ws.send('Connected!');
+  ws.on('message', function incoming(title) {
+    if (!title || lastSong === title || title === "YouTube") return;
+    lastSong = title;
+    discord.send(title);
+  });
+  ws.send('Connected!');
 });
