b7cf3bb53547517de6187f2e2cebde0ba41f7c92
Daniel Dimitrov <ayyayyedev@gmail.com>
2019-03-25 16:20:44 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,3 +0,0 @@
-start Powershell.exe -command "pkg server.js --output AyyTybe-server-windows --targets win"
-start Powershell.exe -command "pkg server.js --output AyyTybe-server-linux --targets linux"
-start Powershell.exe -command "pkg server.js --output AyyTybe-server-macos --targets macos"
